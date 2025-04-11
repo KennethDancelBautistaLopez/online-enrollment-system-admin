2959bb0837b4f064d686a95d2b517a8bca6c8669
knnthzuu? <lopez.kennethdancel@gmail.com>
2025-04-11 14:30:44 +0800

    added icon

--- components/Nav.js
+++ components/Nav.js
@@ -89,7 +89,12 @@ export default function Nav({show}) {
               Total Students
             </Link>
 
-            <Link href={'/all-payments'} className={pathname.includes('/all-payments') ? activeLink : inactiveLink}>List of Payments</Link>
+            <Link href={'/all-payments'} className={pathname.includes('/all-payments') ? activeLink : inactiveLink}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={pathname.includes('/all-payments') ? activeIcon : inactiveIcon}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
+          </svg>
+
+            List of Payments</Link>
             {session?.user?.role === 'superAdmin' && (
           <>  
             <Link href={'/payments'} className={pathname.includes('/payments') ? activeLink : inactiveLink}>
