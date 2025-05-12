74a26577055e0eceb6b3f74bef5fabaf8fa2de48
knnthzuu? <lopez.kennethdancel@gmail.com>
2025-05-12 17:58:43 +0800

    fixed some error in subjects and sections

--- components/Nav.js
+++ components/Nav.js
@@ -137,12 +137,16 @@ export default function Nav({show}) {
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
           </svg>
           List of Payments</Link>
+          {session?.user?.role === 'superAdmin' && (
+          <>  
           <Link href={'/paymentsPage'} className={pathname.includes('/paymentsPage') ? activeLink : inactiveLink}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={pathname.includes('/paymentsPage') ? activeIcon : inactiveIcon}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M14.121 7.629A3 3 0 0 0 9.017 9.43c-.023.212-.002.425.028.636l.506 3.541a4.5 4.5 0 0 1-.43 2.65L9 16.5l1.539-.513a2.25 2.25 0 0 1 1.422 0l.655.218a2.25 2.25 0 0 0 1.718-.122L15 15.75M8.25 12H12m9 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
             Payments
           </Link>
+          </>
+          )}
         {session?.user?.role === 'superAdmin' && (
           <Link href={'/create-admin'} className={pathname.includes('/create-admin') ? activeLink : inactiveLink}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={pathname.includes('/create-admin') ? activeIcon : inactiveIcon}>
