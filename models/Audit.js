33b2f54d94148e8eafacc042c68c32738cebb492
KurtJQ <irockstar12230@gmail.com>
2025-05-17 16:02:23 +0800

    changed user object to use email instead of name

--- models/Audit.js
+++ models/Audit.js
@@ -13,8 +13,8 @@ const AuditLogSchema = new mongoose.Schema(
     timestamp: { type: Date, default: Date.now },
 
     user: {
-      id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      name: String,
+      id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      email: { type: String, required: true },
     },
 
     before: { type: mongoose.Schema.Types.Mixed },
